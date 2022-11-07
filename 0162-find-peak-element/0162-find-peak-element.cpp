8ead80e55ac26779ed3ca210a6326659346ca7f0
Pulkit Srivastava <105780134+PULKlT@users.noreply.github.com>
2022-11-08 02:24:00 +0530

    Time: 18 ms (5.42%), Space: 8.9 MB (54.27%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int mid,lo=0,hi=nums.size()-1;
+        
+        while(lo<hi){
+            mid = lo + (hi-lo)/2;
+            if(nums[mid]>nums[mid+1]){
+                hi=mid;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return lo;
+    }
+};
