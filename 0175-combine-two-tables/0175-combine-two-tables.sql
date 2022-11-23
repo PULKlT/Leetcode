8b603ebd59390a8354c1c20b8f4247654ddaa972
Pulkit Srivastava <105780134+PULKlT@users.noreply.github.com>
2022-11-23 12:51:56 +0530

    Time: 519 ms (44.18%), Space: 0B (100.00%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -2,4 +2,6 @@
 #select p.firstName, p.lastName, a.city, a.state
 #from Person p left join Address a
 #on p.PersonId = a.PersonId;
-select FirstName, LastName, City, State from Person left join Address on Person.PersonId = Address.PersonId;
+select FirstName, LastName, City, State 
+from Person left join Address 
+on Person.PersonId = Address.PersonId;
