00c6527387a129036bf6de6449bcf782ffc97190
Pulkit Srivastava <105780134+PULKlT@users.noreply.github.com>
2022-11-24 01:29:24 +0530

    Time: 1108 ms (90.07%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,9 +1,10 @@
 # Write your MySQL query statement below
 select distinct(s.name)
-from salesperson s
+from salesperson s 
+# left join orders o on s.sales_id=o.sales_id
 where 
 # o.com_id is null
-# or 
+# or o.sales_id not in 
 s.sales_id not in 
 (select sales_id from orders where com_id = (select com_id from company where name = 'RED'))
 ;
