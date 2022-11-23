327796e9995f885a01e2788aff77a209b5ed98a0
Pulkit Srivastava <105780134+PULKlT@users.noreply.github.com>
2022-11-24 01:28:26 +0530

    Time: 2279 ms (17.15%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,8 +1,20 @@
 # Write your MySQL query statement below
 select distinct(s.name)
 from salesperson s
-left join orders o on s.sales_id=o.sales_id
-where o.com_id is null
-or o.sales_id not in 
+where 
+# o.com_id is null
+# or 
+s.sales_id not in 
 (select sales_id from orders where com_id = (select com_id from company where name = 'RED'))
 ;
+
+
+# select salesperson.name
+# from orders o join company c on (o.com_id = c.com_id and c.name = 'RED')
+# right join salesperson on salesperson.sales_id = o.sales_id
+# where o.sales_id is null;
+# 
+# Interesting solution...to explain for others...
+# The first inner join creates a view for just 'RED' orders
+# The right join ensures all salespersons are included (even those who do not have RED orders)
+# The WHERE IS NULL gives salespersons who did not have any RED orders due to the right join
