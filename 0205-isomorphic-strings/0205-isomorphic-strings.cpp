8e0003e642ee7fa4180b4d2243c78749786923cb
Pulkit Srivastava <105780134+PULKlT@users.noreply.github.com>
2022-11-26 13:12:57 +0530

    Time: 18 ms (41.02%), Space: 6.8 MB (98.47%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    bool h(string s,string t){
-        map<char,char> mp;
+//     bool h(string s,string t){
+//         map<char,char> mp;
         
-        for(int i=0;i<s.size();i++){
-            if(mp.find(s[i])!=mp.end())    
-            {
-                if(mp[s[i]]!=t[i])    return false;
-            }    
-            else    mp[s[i]]=t[i];
+//         for(int i=0;i<s.size();i++){
+//             if(mp.find(s[i])!=mp.end())    
+//             {
+//                 if(mp[s[i]]!=t[i])    return false;
+//             }    
+//             else    mp[s[i]]=t[i];
+//         }
+//         return true;
+//     }
+    bool isIsomorphic(string s, string t) {
+        // return h(s,t)&&h(t,s);
+        int m1[256] = {0}, m2[256] = {0}, n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (m1[s[i]] != m2[t[i]]) return false;
+            m1[s[i]] = i + 1;
+            m2[t[i]] = i + 1;
         }
         return true;
-    }
-    bool isIsomorphic(string s, string t) {
-        return h(s,t)&&h(t,s);
     }
 };
