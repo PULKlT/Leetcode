2dedc648c514432b8795fd4f38c5dbb880990afd
Pulkit Srivastava <105780134+PULKlT@users.noreply.github.com>
2022-11-26 12:26:06 +0530

    Time: 41 ms (71.37%), Space: 31.1 MB (87.11%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -4,25 +4,19 @@ public:
         int rsum=0,lsum=0;
         int ans =-1;
         if(nums.size()==1)  return 0;
-//         for(int i=1;i<nums.size();i++){
-//             rsum+=nums[i];
-//         }
-//         for(int i=0;i<nums.size();i++){
-//             if(lsum==rsum){
-//                 return i;
-//             }
-//             lsum+=nums[i];
-//             if(i+1==nums.size())
-//                 rsum = 0;
-//             else
-//                 rsum-=nums[i+1];
-            
-//         }
+        for(int i=1;i<nums.size();i++){
+            rsum+=nums[i];
+        }
         for(int i=0;i<nums.size();i++){
-            // lsum = 
-            if(accumulate(nums.begin(),nums.begin()+i,lsum)==accumulate(nums.begin()+i+1,nums.end(),rsum)){
+            if(lsum==rsum){
                 return i;
             }
+            lsum+=nums[i];
+            if(i+1==nums.size())
+                rsum = 0;
+            else
+                rsum-=nums[i+1];
+            
         }
         return ans;
     }
