cc0ae8d75214d41e107790054b692452d62d7c9c
Pulkit Srivastava <105780134+PULKlT@users.noreply.github.com>
2022-11-28 18:33:11 +0530

    Time: 650 ms (24.00%), Space: 0B (100.00%) - LeetHub

---
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+# select distinct(a.actor_id), a.director_id
+# from ActorDirector a, ActorDirector b
+# where  
+# a.actor_id = b.actor_id and a.director_id = b.director_id and
+# a.timestamp <> b.timestamp
+# and (select count(c.actor_id) from ActorDirector c where c.actor_id=a.actor_id group by actor_id) > 2
+# # and (select count(c.director_id) from ActorDirector c where c.director_id=a.director_id group by director_id) > 2
+# group by actor_id
+# ;
+
+select actor_id, director_id
+from ActorDirector
+group by 1,2
+having count(1)>=3
