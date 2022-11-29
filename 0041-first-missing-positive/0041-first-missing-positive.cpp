844884bd77a6f2f88b94014e46bb954cc2244285
Pulkit Srivastava <105780134+PULKlT@users.noreply.github.com>
2022-11-30 02:29:41 +0530

    Time: 341 ms (5.06%), Space: 47.7 MB (7.04%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        // if(nums.size()==100000) return 100001;
+        set <int> st;
+        for(int i=0;i<nums.size();i++){
+            st.insert(nums[i]);
+        }
+        int i = 1;
+        for(int i=1;i<=st.size()+1;i++){
+            if(st.find(i)==st.end())
+                return i;
+        }
+        return -1;
+        
+    }
+};
