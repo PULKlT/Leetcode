091d939b1323f4bd6ddf44d6108ee1196c2ad4dd
Pulkit Srivastava <105780134+PULKlT@users.noreply.github.com>
2023-01-02 16:02:45 +0530

    Time: 5 ms (24.37%), Space: 6.2 MB (24.57%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int count = 0;
-        for(int i=0;i<word.size()-1;i++)
-        {
-            if(count>1&&islower(word[i+1]))
+        // int count = 0;
+        // for(int i=0;i<word.size()-1;i++)
+        // {
+        //     if(count>1&&islower(word[i+1]))
+        //         return false;
+        //     if(islower(word[i])){
+        //         if(isupper(word[i+1]))
+        //             return false;
+        //     }
+        //     else
+        //         count++;
+        // }
+        // if(count>1&&islower(word[word.size()-1]))
+        //         return false;
+        // return true;
+        for(int i=1;i<word.size();i++){
+            if( (isupper(word[1]) != isupper(word[i]))  ||  (islower(word[0]) && isupper(word[i])) )
                 return false;
-            if(islower(word[i])){
-                if(isupper(word[i+1]))
-                    return false;
-            }
-            else
-                count++;
         }
-        if(count>1&&islower(word[word.size()-1]))
-                return false;
         return true;
     }
 };
