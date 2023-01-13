cc793f0031b01fc8795cae18a4699d66a77fcede
Pulkit Srivastava <105780134+PULKlT@users.noreply.github.com>
2023-01-13 14:35:24 +0530

    Time: 5 ms (79.94%), Space: 7.4 MB (81.62%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    // bool Pal(string s,int i,int j)
+    // {
+    //     if(i>=j)
+    //         return true;
+    //     if(s[i]!=s[j])
+    //         return false;
+    //     else
+    //         return Pal(s,i+1,j-1);
+    // }
+    bool isPalindrome(string s) {
+        // string st;
+        int i=0,j=s.size()-1;
+        // for(auto i:s)
+        // {
+        //     if(isalnum(i))
+        //         st.push_back(tolower(i));
+        // }
+        //     return Pal(st,0,st.size()-1);
+        for(i=0;i<=j;i++){
+            while(!isalnum(s[i])&&i<j) i++;
+            if(i==j)
+                return true;
+            while(!isalnum(s[j])&&j>0) j--;
+            char x=tolower(s[i]),y=tolower(s[j]);
+            // cout<<x<<" "<<y<<endl;
+            if(x!=y)
+                return false;
+            j--;
+        }
+        return true;
+    }
+};
