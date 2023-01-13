1db1925e871ee5ad06064e17cb60eacc471ad5f4
Pulkit Srivastava <105780134+PULKlT@users.noreply.github.com>
2023-01-13 14:38:10 +0530

    Time: 7 ms (70.06%), Space: 8 MB (9.74%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -11,25 +11,29 @@ public:
     //         return Pal(s,i+1,j-1);
     // }
     bool isPalindrome(string s) {
-        // string st;
+        string st;
         int i=0,j=s.size()-1;
-        // for(auto i:s)
-        // {
-        //     if(isalnum(i))
-        //         st.push_back(tolower(i));
-        // }
-        //     return Pal(st,0,st.size()-1);
-        for(i=0;i<=j;i++){
-            while(!isalnum(s[i])&&i<j) i++;
-            if(i==j)
-                return true;
-            while(!isalnum(s[j])&&j>0) j--;
-            char x=tolower(s[i]),y=tolower(s[j]);
-            // cout<<x<<" "<<y<<endl;
-            if(x!=y)
-                return false;
-            j--;
+        for(auto i:s)
+        {
+            if(isalnum(i))
+                st.push_back(tolower(i));
         }
-        return true;
+        string str = st;
+        reverse(str.begin(),str.end());
+        return(st==str);
+            // return Pal(st,0,st.size()-1);
+        
+        // for(i=0;i<=j;i++){
+        //     while(!isalnum(s[i])&&i<j) i++;
+        //     if(i==j)
+        //         return true;
+        //     while(!isalnum(s[j])&&j>0) j--;
+        //     char x=tolower(s[i]),y=tolower(s[j]);
+        //     // cout<<x<<" "<<y<<endl;
+        //     if(x!=y)
+        //         return false;
+        //     j--;
+        // }
+        // return true;
     }
 };
